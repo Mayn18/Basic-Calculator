b2f2481b38318bc386b63b27f3056e3b85adcf7b
Mayn18 <maynaik1865@gmail.com>
2026-07-17 19:24:19 +0530

    Added git ignore

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const input = document.querySelector("#text-field");
+const numberButtons = document.querySelectorAll(".num-buttons");
+const operatorButtons = document.querySelectorAll(".operator-buttons");
+const functionButtons = document.querySelectorAll(".function-buttons");
+
+console.log(numberButtons, operatorButtons, functionButtons);
 
 input.addEventListener("keydown", (event) => {
     if(event.key === "Enter"){
@@ -8,6 +13,6 @@ input.addEventListener("keydown", (event) => {
 }
 );
 
-
+calculate(){}
 
 
